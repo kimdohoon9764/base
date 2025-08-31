679df175c89537d65f1a2ea0d77474ae11b110ff
kimdohoon9764 <kdh970604@naver.com>
2025-08-31 13:22:09 +0900

    block_chain_exmaple_1

---
+++ 블록체인/blcok_example.py
@@ -0,0 +1,55 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, nonce=0):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        value = f"{self.index}{self.previous_hash}{self.timestamp}{self.data}{self.nonce}"
+        return hashlib.sha256(value.encode()).hexdigest()
+
+    def mine_block(self, difficulty):
+        while self.hash[:difficulty] != "0" * difficulty:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+        print(f"Block mined: {self.hash}")
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = 4  # Adjust mining difficulty
+
+    def create_genesis_block(self):
+        return Block(0, "0", time.time(), "Genesis Block")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.mine_block(self.difficulty)
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i - 1]
+
+            if current.hash != current.calculate_hash():
+                return False
+            if current.previous_hash != previous.hash:
+                return False
+        return True
+
+# 사용 예시
+my_chain = Blockchain()
+my_chain.add_block(Block(1, "", time.time(), "데이터 1"))
+my_chain.add_block(Block(2, "", time.time(), "데이터 2"))
+
+print("체인이 유효한가요?", my_chain.is_chain_valid())
