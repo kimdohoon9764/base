fb52b6c89d9b440c7002d4afe47a00749dc3619f
kimdohoon9764 <kdh970604@naver.com>
2025-09-01 22:39:24 +0900

    java Buffered_*

---
+++ JAVA/Chapter3_1.java
@@ -0,0 +1,18 @@
+package JAVA;
+import java.io.*;
+import java.util.Scanner;
+public class Chapter3_1 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int b= Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(b));
+        bw.flush();
+        
+    }
+}
