8a665a8b2dc7169e94f42293afb36a3404a5059c
kimdohoon9764 <kdh970604@naver.com>
2025-09-04 13:41:03 +0900

    python Sort

---
+++ Python/Sort_descending.py
@@ -0,0 +1,9 @@
+unsorted_list=[5,3,4,2,1]
+
+
+sorted_list=sorted(unsorted_list)
+reversed_list=sorted(unsorted_list,reverse=True)
+print(unsorted_list)
+
+print("sorted_list : %s"%sorted_list)
+print("reversed_list : %s"%reversed_list)
